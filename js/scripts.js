b7cea37af483dbd7c4bdf289ce14fb4dec40a822
Kate Kiatsiri <keidsiri@gmail.com>
2021-11-01 13:44:26 -0700

    Initialize the practice lesson on P.14 , Wk.4

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,6 @@ $(document).ready(function() {
   $("#place1").click(function() {
     $("#id1").empty();
     $("#id1").toggle();
-    
     $("#id1").append("<li>" + portland.location + "</li>");
     $("#id1").append("<li>" + portland.landmark + "</li>");
     $("#id1").append("<li>" + portland.time + "</li>");
